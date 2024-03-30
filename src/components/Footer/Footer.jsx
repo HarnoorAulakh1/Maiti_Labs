eac05f2c3a26124fcc5c24c710e63fb15b6ade12
Harnoor Singh Aulakh <harnoorsinghaulakh@Harnoors-MacBook-Air.local>
2024-03-30 22:27:49 +0530

    phone mode added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,20 +6,26 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.flex}>
-      <div className={styles.logo}>
-        <div className={styles.frame}>
-          <div className={styles.img}></div>
+        <div className={styles.logo}>
+          <div className={styles.frame}>
+            <div className={styles.img}></div>
+          </div>
+          <p>DeCarbonn</p>
         </div>
-        <p>DeCarbonn</p>
-      </div>
-      <p className={styles.query}>Have any query?</p>
-      <Mailchimp />
+        <p className={styles.query}>Have any query?</p>
+        <Mailchimp />
       </div>
       <p className={styles.endText}>Made with ❤️ at MaitiLabs</p>
       <div className={styles.stacks}>
-        <Stacks heading="SOCIAL" items={["Linkedin","Instagram","Facebook"]}/>
-        <Stacks heading="COMPANY" items={["Home","Project","About"]}/>
-        <Stacks heading="INNITIATIVES" items={["Maiti Labs","DeCarbonn","GreenBit"]}/>
+        <Stacks
+          heading="SOCIAL"
+          items={["Linkedin", "Instagram", "Facebook"]}
+        />
+        <Stacks heading="COMPANY" items={["Home", "Project", "About"]} />
+          <Stacks
+            heading="INNITIATIVES"
+            items={["Maiti Labs", "DeCarbonn", "GreenBit"]}
+          />
       </div>
       <p className={styles.copyright}> copyright © 2024 DeCarbonn</p>
     </footer>
--- src/components/Footer/Mailchimp.jsx
+++ src/components/Footer/Mailchimp.jsx
@@ -3,7 +3,7 @@ function Mailchimp() {
   return (
     <div className={styles.mailchimp}>
       <input type="text" placeholder="Enter your email" />
-      <button className={styles.btn}>Subscribe</button>
+      <button className={styles.btn}><p>Subscribe</p></button>
     </div>
   );
 }
